58c5484b505866cca7fe906f3f51ffffb0fc2cf6
minigalieva <Elmira-Minigalieva@yandex.ru>
2016-02-21 12:22:12 +0600

    Hello#

---
+++ anonymous/anonymous/Construction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous
+{
+    public class Construction : IConstruction
+    {
+        public IMatrix MakeProfile(double[] au, double[] al, double[] di, int[] ia, int size_au_al, int size_di, int size_ia)
+        {
+            return new ProfileMatrix(au, al, di, ia, size_au_al, size_di, size_ia);
+        }
+        public Vector MakeVector(int size, double[] values)
+        {
+            return new Vector(size, values);
+        }
+    }
+}
---
+++ anonymous/anonymous/IMatrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace anonymous
+{
+    public interface IMatrix
+    { 
+        Vector Multiply(Vector x); //перемножение матрицы на вектор
+        Vector TMultiply(Vector x);//перемножает транспонированную матрицу на вектор
+        double abs_discrepancy(Vector x, Vector F);//абсолютная невязка
+        double rel_discrepancy(Vector x, Vector F);//относительная невязка
+    }   
+}
