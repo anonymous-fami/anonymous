2529b946089bdba8d0830a28e72e026b3b3f8da9
hatabitch <nsk_serega@bk.ru>
2016-02-29 02:01:59 +0600

    Добавлено неполное разложение LLt

--- anonymous/anonymous/Preconditioner.cs
+++ anonymous/anonymous/Preconditioner.cs
@@ -61,13 +61,66 @@ namespace anonymous
 
         public void createLLT(ProfileMatrix matrix, out ProfileMatrix out_l)
         {
+            int[] ia = new int[matrix.N + 1];
+            for (int i = 0; i < matrix.N + 1; i++)
+            {
+                ia[i] = matrix.Ia[i];
+            }
+            double[] L = new double[ia[matrix.N] - 1];
+            double[] Di = new double[matrix.N];
+            double epsForFirstElem = 1e-5;
+            if(Math.Abs(matrix.Di[0])< epsForFirstElem)
+            {
+                for (int i = 0; i < matrix.N; i++)
+                {
+                    Di[i] = matrix.Di[i] + epsForFirstElem;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < matrix.N; i++)
+                {
+                    Di[i] = matrix.Di[i];
+                }
+            }
+            for (int i = 0; i < matrix.N; i++)
+            {
+                double sumDi = 0;
+                for (int j = ia[i]; j < ia[i + 1]; j++)
+                {
+                    int length = ia[i + 1] - ia[i];
+                    int k = 0;
+                    int stolb = i - (ia[i + 1] - j);
+                    double sumL = 0;
+                    for (k = 0; k < j - ia[i]; k++) 
+                    {
+                        //int temp = i - length + k;
+                        //int temp2 = stolb - (ia[stolb + 1] - ia[stolb]);
+                        //if (stolb + k - (ia[stolb + 1] - ia[stolb]) - (i - length + k) > 0)
+                        //   continue;
+                        int temp1 = 0;
+                        while (stolb + temp1 - (ia[stolb + 1] - ia[stolb]) != (i - length + k)
+                            && (ia[stolb + 1] - ia[stolb]) != 0)
+                        {
+                            temp1++;
+                            continue;
+                        }
+                        if (ia[stolb + 1] - ia[stolb] == 0) continue;
+                        sumL += L[ia[i] + k] * L[ia[stolb] + temp1];    // L[i,j]=1/L[j,j](A[i,j]-(SUM(L[i,k]*L[j,k]),K=1 to j-1))
+                    }
+                    L[j] = (matrix.Al[j] - sumL) / Di[stolb] ;
+                    sumDi += L[j] * L[j];
+                }
+                Di[i] = Math.Sqrt(Di[i] - sumDi);
+            }
+
             throw new NotImplementedException();
         }
 
 
 
         public void createLU(ProfileMatrix matrix, out ProfileMatrix out_l, out ProfileMatrix out_u)
-        {
+        {            
             throw new NotImplementedException();
         }
     }
