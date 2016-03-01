7fd7e2c00b5369d03aee50bbc6c94a5a24c8cdb0
Foootbaler <Student0prk@gmail.com>
2016-03-02 01:34:46 +0600

    Добавлены интерфейс Isolver, реализация классов МСГ и ЛОС

---
+++ anonymous/anonymous/Isolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous
+{
+    public interface Isolver
+    {
+        public Vector Solve(ProfileMatrix ProfMatr, Vector RightPart, Vector Initial, int maxiter, double eps); 
+    }
+}
